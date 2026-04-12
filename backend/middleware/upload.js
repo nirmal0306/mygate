87faf202ed0a2a4ad344fc946f0eeb6cec413256
Nirmal Barot <162555901+nirmal0306@users.noreply.github.com>
2026-04-12 17:20:34 +0530

    upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,12 +1,41 @@
+// const multer = require("multer");
+// const { CloudinaryStorage } = require("multer-storage-cloudinary");
+// const cloudinary = require("../config/cloudinary");
+
+// const storage = new CloudinaryStorage({
+//   cloudinary,
+//   params: {
+//     folder: "mygate",
+//     allowed_formats: ["jpg", "png", "jpeg"],
+//   },
+// });
+
+// const upload = multer({ storage });
+
+// module.exports = upload;
+
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../config/cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: {
-    folder: "mygate",
-    allowed_formats: ["jpg", "png", "jpeg"],
+  params: (req, file) => {
+    let folder = "uploads/others";
+
+    // 🔥 Detect route
+    if (req.originalUrl.includes("residents")) {
+      folder = "uploads/residents";
+    } else if (req.originalUrl.includes("visitors")) {
+      folder = "uploads/visitors";
+    } else if (req.originalUrl.includes("security")) {
+      folder = "uploads/security";
+    }
+
+    return {
+      folder: folder,
+      allowed_formats: ["jpg", "png", "jpeg"],
+    };
   },
 });
 
